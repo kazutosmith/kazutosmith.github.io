1a4aefd2a20fd34263f8a8513ab4c94dc7ae15ea
Raiymbek Mustazhapov <raimbek.riko@gmail.com>
2023-03-10 17:38:07 +0600

    am jeme

---
+++ js/index.js
@@ -0,0 +1,589 @@
+var names = [{
+    number: 1,
+    fact: `
+      Girls gather at a mansion to see.
+      In their hands, they each hold a key,
+      To a place where anything can be,
+      And so they step inside with glee.
+
+      A delivery guy arrives at the door,
+      And the girls invite him in to explore,
+      With a strange smile and a glint in their eye,
+      They offer him a slice of a pie.
+
+      But as he takes a bite, he starts to feel,
+      A strange sensation that seems unreal,
+      As the girls close in , he tries to flee,
+      But they capture him with eerie ease.
+      `,
+    options: "",
+    answer: "Red Velvet - Peekaboo"
+  },
+  {
+    number: 2,
+    fact: `
+      In a kitchen of neon lights,
+      These boys prepare to cook tonight.
+      Dressed in chef hats and aprons too,
+      They 're ready to show what they can do.
+
+      The music starts, the beat is strong,
+      Ingredients and utensils fly along.
+      Vegetables chopped and pots boiling over,
+      The boys dance and rap like there 's no other.
+
+      Stunts and tricks, they do it all,
+      Riding food carts, standing on ladles so tall.
+      Culinary ninjas, chopping in mid - air,
+      Their moves are smooth, with utmost care.
+
+    `,
+    options: "",
+    answer: "SKZ - God's Menu"
+  },
+  {
+    number: 3,
+    fact: `
+    The music builds, the camera pans,
+    To giant statues, of each band member stands,
+    A symbol of their greatness, their reach so grand,
+    A message clear, they're here to command.
+
+    In one scene, they stand upon a throne,
+    Surrounded by a sea of human heads, they're shown,
+    Their fans, a fervent, loyal force, to own,
+    A testament to the band's fame grown.
+
+    The colors burst, the scenes so bright,
+    A celebration of culture, a story in sight,
+    In every frame, a message clear,
+    To love yourself, to conquer fear.
+    `,
+    options: "",
+    answer: "BTS - IDOL"
+  },
+  {
+    number: 4,
+    fact: `
+    In a land so dreamy and bright,
+    The boys of this band take flight,
+    To serenade a girl so fine,
+    In the music video so divine
+
+    From a tiny house, they appear,
+    And as they sing, they shed no tear,
+    For they know they'll win her heart,
+    With their charm, wit, and musical art.
+
+    They transform into tiny men,
+    To make the girl feel small again,
+    But as they grow big, they show her how,
+    To embrace her beauty, here and now.
+    `,
+    options: "",
+    answer: "GOT7 - Just Right"
+  },
+  {
+    number: 5,
+    fact: `
+    In a post-apocalyptic land,
+    Where zombies roam the streets,
+    A girl group takes a stand,
+    And their beats are hard to beat.
+
+    [Nope] is the name of the crew,
+    Their moves are smooth and slick,
+    As they sing and dance anew,
+    Their fans can't help but stick.
+
+    The zombies gather around,
+    But the girls don't seem back down,
+    Their feet stomp on the ground,
+    And they twirl with a graceful gown.
+    `,
+    options: "",
+    answer: "TWICE - Ooh-Ahh"
+  },
+  {
+    number: 6,
+    fact: `
+    In the music video [something]-op
+    The band takes us on a tour non-stop
+    With a bamboo forest, a tropical resort, and a beach
+    Each scene is so unique, it's beyond the reach
+
+    In the bamboo forest, the boys dance
+    Surrounded by greenery, they prance
+    The 3D animations come to life
+    Creating a world that's beyond the strife
+
+    The bamboo shoots tower high
+    As the boys dance with moves so sly
+    The animations add an extra touch
+    Creating a world that's good so much
+    `,
+    options: "",
+    answer: "EXO - Ko Ko Bop"
+  },
+  {
+    number: 7,
+    fact: `
+    In the music video we will look on
+    The boys bring a quirky vibe that's never gone
+    With exaggerated expressions and silly moves
+    They bring a smile to our faces that's hard to remove
+
+    As they sing and dance, we notice something new
+    Each one holds a bottle, it's quite a view
+    The bottles are empty with different labels
+    As they hold them up, we wonder what it enables
+
+    But then we realize, it's all for fun
+    The empty bottles are a part of their pun
+    They use them to emphasize their stress
+    And to remind us to let go, to not obsess
+    `,
+    options: "",
+    answer: "Big Byung (Big Bottle) - Stress Come on!"
+  },
+  {
+    number: 8,
+    fact: `
+    Enter through the gates of this grand estate,
+    The boys awaits with an invitation so great.
+    My realm they call it, a place to let loose,
+    A home filled with music, laughter, and no excuse.
+
+    Take in the opulence of every room and hall,
+    Admire the intricate details, and feel ten feet tall.
+    Boys show off their dance moves with little ease,
+    As the bass thumps and the crowd agrees.
+
+    This is more than just a song,
+    It's an invitation to come along,
+    To the world of hospitality, where music reigns,
+    And where their house becomes yours, without a change.
+    `,
+    options: "",
+    answer: "2PM - My House"
+  },
+  {
+    number: 9,
+    fact: `
+    The boys are dressed in black and white
+    Ready to fight with all their might
+    They face off against their foes
+    In a dance battle with epic flows
+
+    Each member takes their turn to shine
+    Their moves so smooth, they're so divine
+    They jump and spin, kick and punch
+    Their energy never losing much
+
+    With a victorious stance, they declare
+    That they are the ones who will always dare
+    To bring the heat and never quit it
+    That's why they're the ones who truly "[name it]"
+    `,
+    options: "",
+    answer: "NCT - Kick It"
+  },
+  {
+    number: 10,
+    fact: `
+    In this video, a world-renowned tune,
+    He's got the moves that make you swoon.
+    He struts down the street in his flashy suit,
+    Surrounded by ladies in stilettos and boots.
+
+    His special dance is a sensation,
+    Spreading joy and fun to every nation.
+    From the iconic elevator scene,
+    To the beach where he shows his "lean".
+
+    The video's filled with silly humor,
+    From the yoga pants to the elevator rumor.
+    He's got charisma, and he knows how to use it,
+    With his sunglasses and moves he won't lose it.
+    `,
+    options: "",
+    answer: "PSY - Gangnam Style"
+  },
+  {
+    number: 11,
+    fact: `
+    In the land of Korea, long ago,
+    A royal court was filled with woe,
+    For a young prince was cruel and bold,
+    And his rule was harsh, so we're told.
+
+    The people suffered day by day,
+    Forced to obey and never stray,
+    From the strict rules that he'd laid out,
+    And never dared to raise a shout.
+
+    But one day, things began to change,
+    As a hero emerged, fierce and strange,
+    With a modern look, his face so bold,
+    He challenged the prince, so we're told.
+    `,
+    options: "",
+    answer: "August D - Daechwita"
+  },
+  {
+    number: 12,
+    fact: `
+    In this video, a story is told,
+    Of a woman who's brave, independent and bold.
+    With a striking red outfit and fiery hair,
+    She commands the screen with confident flair.
+
+    In every scene, she stands with grace,
+    Her spirit unbroken, in any place.
+    Her solo journey is one of power and might,
+    A testament to her strength, shining bright.
+
+    The castle walls around her may be tall,
+    But her spirit is high, no matter what befall.
+    She is a queen, on her throne, strong and free,
+    Living life on her own terms, as it's meant to be.
+    `,
+    options: "",
+    answer: "Jennie - Solo"
+  },
+  {
+    number: 13,
+    fact: `
+    In the land of K-pop, there's a group so bright,
+    [Name] they call themselves, shining day and night.
+    In their music video today they ask,
+    A question that's simple, yet a challenging task.
+
+    In each room, a different scene unfolds,
+    One girl finds love, another's future is told.
+    There's a secret door, that leads to a surprise,
+    A dance party awaits, right before our eyes.
+
+    As the members strut in their outfits bold,
+    Their eyes searching for what the future holds,
+    An illusion of choice is presented to see,
+    To be or to be, it's a question of destiny.
+    `,
+    options: "",
+    answer: "TWICE - Yes or Yes"
+  },
+  {
+    number: 14,
+    fact: `
+    In the music video that we will log,
+    These members transform like a monologue,
+    One moment they're human, the next a pet,
+    A feline or canine, you can't forget.
+
+    The boys have tails and ears that match their breed,
+    In a world where humans are no longer the lead,
+    They chase each other around and play,
+    Just like real pets, but in a different way.
+
+    So join in the fun and dance along,
+    With these boys and their furry throng.
+    And if you've got a pet of your own,
+    Give them a cuddle and a loving tone.
+    `,
+    options: "",
+    answer: "TXT - Cat & Dog"
+  },
+  {
+    number: 15,
+    fact: `
+    In this music video that’s so hot,
+    The girls set fire to the past they've got,
+    They summon meteorite rain to cleanse their hearts,
+    And break free from society's old, confining parts.
+
+    Their fiery perseverance is on display,
+    As they shed their skins and start anew today,
+    Claiming they’re so bad, they forge ahead,
+    Breaking free from the norms they once dread.
+
+    The powerful beats and electrifying melody,
+    Bring forth their fierce individuality,
+    As they dance with confidence and pride,
+    Their strength shines through, nothing to hide.
+    `,
+    options: "",
+    answer: "LOONA - So What"
+  },
+  {
+    number: 16,
+    fact: `
+    In this music video, a youthful treat,
+    The girls show their moves and infectious beat,
+    With biscuit sounds that are freshly baked,
+    And chocolate chips that can't be faked.
+
+    The song is catchy, the rhythm tight,
+    A sugary rush, a burst of light,
+    The video's a feast, a treat for the soul,
+    A perfect escape, a sweet rock and roll.
+
+    So come and taste this beautiful treat,
+    Will you let the girls move your feet,
+    It's a biscuit beat from the fiery teens,
+    As they call themselves [name].
+    `,
+    options: "",
+    answer: "NewJeans - Cookie"
+  },
+  {
+    number: 17,
+    fact: `
+    In this video we will look in,
+    The girls bring forth a powerful vim.
+    With piercing gazes and fierce stances,
+    They confront their fears and take their chances.
+
+    Their voices soar, their moves are slick,
+    As they declare they won't be tricked.
+    By phobias that once held them back,
+    They're scarelessly on the right track.
+
+    What's your looking at, they demand,
+    As they dance and take command.
+    With confidence, they lead the way,
+    Encouraging you to seize the day.
+    `,
+    options: "",
+    answer: "LE SSERAFIM - Fearless"
+  },
+  {
+    number: 18,
+    fact: `
+    In a world where youth reigns supreme
+    These girls in white step into the scene
+    With a sense of anticipation and a glimmer of hope
+    They sing of a new world, one they'll create and cope
+
+    The world is new, and so are they,
+    But nothing can hold them back, they say.
+    Their dreams are big, their hearts are bold,
+    And they're ready to take on the world
+
+    And so they sing, with all their might and heart
+    Of a new age and era, a brand new start
+    With their voices and their steps, they'll make it all come true
+    For these bright girls, there's nothing they can't do.
+    `,
+    options: "",
+    answer: "SNSD - Into the New World"
+  },
+  {
+    number: 19,
+    fact: `
+    In the video the six girls shine,
+    Their voices soaring, their moves divine.
+    In a lush, green park, they dance and sing,
+    As the world around them comes to life in spring.
+
+    They frolic on a grassy plain,
+    Skipping, jumping, without refrain,
+    Infectious smiles, infectious beat,
+    They make us tap our dancing feet.
+
+    The video's most knockdown scene,
+    Is when they slip and fall, it's quite a scene,
+    But they get right back up and keep on going,
+    Their resilience and determination keep showing.
+  `,
+    options: "",
+    answer: "GFRIEND - Me gustas tu"
+  },
+  {
+    number: 20,
+    fact: `
+    In this music video, we see
+    A girl group bursting with energy.
+    Their bright outfits and synchronized dance
+    Make for a performance that's hard to glance.
+
+    As the beat drops, they command to hit it, ho!
+    A phrase so catchy, it makes you wanna go.
+    Their dance moves, so fluid and astute,
+    Their talent and passion, impossible to refute.
+
+    Tropicana, a theme so bright,
+    Their outfits so colorful, a stunning sight.
+    With backgrounds changing like a commercial break,
+    Their performance, a masterpiece, for our sake.
+    `,
+    options: "",
+    answer: "MOMOLAND - BBoom BBoom"
+  },
+  {
+    number: 21,
+    fact: `
+    In the music video we will show,
+    An icy scene is set to the flow,
+    A fierce girl group in a land of snow,
+    They are unstoppable and aglow.
+
+    Icebergs shimmer in the back,
+    A group of girls now make their mark.
+    The phenomenon has begun,
+    A treat for eye that full of fun.
+
+    Their voices soar, their dance is strong,
+    They won't let you down, they'll keep it on,
+    Till the very end, they won't relent,
+    And you're so done, you're completely spent.
+    `,
+    options: "",
+    answer: "Everglow - DUN DUN"
+  },
+  {
+    number: 22,
+    fact: `
+    So curious, these boys on the case,
+    Investigating clues all over the place.
+    In this music video, they're on a mission,
+    To uncover the truth with precision.
+
+    Each member of the group shines in their own way,
+    And they come together to save the day.
+    Their harmonies soar, their voices unite,
+    And they dance with such grace, it's quite a sight.
+    `,
+    options: "",
+    answer: "SHINEE - Sherlock"
+  },
+  {
+    number: 23,
+    fact: `
+    This music video is a sight to see
+    With beats so fresh and lyrics free
+    In the video, we got all eyes on her
+    With a gang of friends, all cool as fur
+
+    She's rocking in leather and lace so bold
+    And chains and studs that never get old
+    But something catches my eye, I confess
+    A little detail that I must address
+
+    In the video, there's a moment of tease
+    Where she’s seen walking dogs with ease
+    But these dogs are no ordinary pets
+    They're human on a leash, no need to fret
+    `,
+    options: "",
+    answer: "Hyuna - Roll Deep"
+  },
+  {
+    number: 24,
+    fact: `
+    In this amazing music video,
+    Girls strut with a powerful ego,
+    All clad in stunning black attire,
+    Their confidence igniting like fire.
+
+    Suddenly, a member starts to snip,
+    Her long hair falls in a clip,
+    The scissors cut through the air,
+    As she transforms with daring flair.
+
+    The others follow with cutting shears,
+    As their hair falls without any tears,
+    Their new looks bold and chic,
+    Adding edge to their already sleek.
+    `,
+    options: "",
+    answer: "CLC - Black Dress"
+  },
+  {
+    number: 25,
+    fact: `
+    In the video the gaze we lay,
+    A feline tale unfolds to play,
+    With sleek moves and sultry glances,
+    They prowl through the night's darkened dances.
+
+    In a heist they're about to make,
+    The girls slink and slither, for goodness sake!
+    To steal a large diamond, shining bright,
+    Their skills on full display with delight.
+
+    With seductive gazes and purrs so sweet,
+    This music video is quite the treat,
+    Their heist complete, the diamond's won,
+    The girls slink off, a job well done.
+    `,
+    options: "",
+    answer: "AOA - Like a Cat"
+  }
+];
+
+var block = $(".cards");
+
+for (var i = 0; i < names.length; i++) {
+  block.append(
+    '<div class=" card [ is-collapsed ] ">' +
+    '<div class="card__inner [ js-expander ]">' +
+    '<span class="num">' +
+    (i + 1) +
+    "</span>" +
+    "</div>" +
+    '<div class="card__expander">' +
+    '<h1 class="fact_text"><pre>' +
+    names[i].fact +
+    "</pre></h1>" +
+    '<h2 class="options_text">' +
+    names[i].options +
+    "</h2>" +
+    '<h3 class="show_a">Показать ответ</h3>' +
+    '<i class="fa fa-close [ js-collapser ]"></i>' +
+    "</div>" +
+    "</div>"
+  );
+}
+
+var $cell = $(".card");
+
+//open and close card when clicked on card
+$cell.find(".js-expander").click(function () {
+  var $thisCell = $(this).closest(".card");
+
+  if ($thisCell.hasClass("is-collapsed")) {
+    $cell
+      .not($thisCell)
+      .removeClass("is-expanded")
+      .addClass("is-collapsed")
+      .addClass("is-inactive");
+    $thisCell.removeClass("is-collapsed").addClass("is-expanded");
+
+    if ($cell.not($thisCell).hasClass("is-inactive")) {
+      //do nothing
+    } else {
+      $cell.not($thisCell).addClass("is-inactive");
+    }
+  } else {
+    $thisCell.removeClass("is-expanded").addClass("is-collapsed");
+    $thisCell.find(".card__inner").css("background-color", "#ababab");
+    $cell.not($thisCell).removeClass("is-inactive");
+  }
+});
+
+//close card when click on cross
+$cell.find(".js-collapser").click(function () {
+  var $thisCell = $(this).closest(".card");
+
+  $thisCell.removeClass("is-expanded").addClass("is-collapsed");
+  $thisCell.find(".card__inner").css("background-color", "#5F4B8B");
+  $cell.not($thisCell).removeClass("is-inactive");
+});
+
+$cell.find(".show_a").click(function () {
+  var $thisCell = $(this).closest(".card");
+  var num = $thisCell.find(".num")[0].innerHTML;
+  var answer = names[num - 1].answer;
+  $thisCell.find(".options_text").html(answer);
+  $thisCell.find(".options_text").addClass("answer_shown");
+});
